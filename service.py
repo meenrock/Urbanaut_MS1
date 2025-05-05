db4bd3de15f2fd1941da549eb3794093a6eb4efc
sstacker_m <77928053+meenrock@users.noreply.github.com>
2025-05-05 16:55:51 +0700

    asdas

--- service.py
+++ service.py
@@ -1,9 +1,16 @@
+import datetime
+
 import bentoml
 import numpy as np
 from bentoml._internal.io_descriptors import NumpyNdarray
+from grpc import services
+from pandas import DataFrame
+import geopandas as gpd
 
 from sklearn import svm
 from sklearn import datasets
+
+from src.data_processing.models.svm_land_use import SVMLandUseClassifier
 
 # Load training data set
 iris = datasets.load_iris()
@@ -13,14 +20,37 @@ X, y = iris.data, iris.target
 clf = svm.SVC(gamma='scale')
 clf.fit(X, y)
 
+my_model = SVMLandUseClassifier()
+my_model.train()
+
 # Save model to the BentoML local Model Store
 saved_model = bentoml.sklearn.save_model("iris_clf", clf)
 
 iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
+area_sqm_pred_runner = bentoml.sklearn.get("area_sqm_svm_clf:latest").to_runner()
 
-svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
+svc = bentoml.Service("iris_classifier",
+                      runners=[iris_clf_runner, area_sqm_pred_runner],
+                      )
+
 
 @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
 def classify(input_series: np.ndarray) -> np.ndarray:
     result = iris_clf_runner.predict.run(iris.data)
+    return result
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def areasqmsvm(input_series: np.ndarray) -> np.ndarray:
+    # input_series: np.ndarray
+    filePath = "D:/HighSpeedStorage/LVCHighSpd/MeenMookCoProject/POC/research/land-use-data/Landuse_bkk/กรุงเทพมหานคร2566/การใช้ที่ดิน/LU_BKK_2566.shp"
+    data = gpd.read_file(filePath)
+    area_sqm = data['Area_Sqm']
+    input_series = DataFrame({'area_sqm': area_sqm})
+    result = area_sqm_pred_runner.predict.run(input_series)
+    # result = self.area_sqm_svm_clf.predict.run(input_series)
+    # return {
+    #     "prediction": return_result.tolist(),
+    #     "model": "trend_transformer",
+    #     "timestamp": datetime.datetime.now().isoformat()
+    #     }
     return result
--- src/data_processing/models/svm_land_use.py
+++ src/data_processing/models/svm_land_use.py
@@ -11,19 +11,19 @@ class SVMLandUseClassifier:
         filePath = "D:/HighSpeedStorage/LVCHighSpd/MeenMookCoProject/POC/research/land-use-data/Landuse_bkk/กรุงเทพมหานคร2566/การใช้ที่ดิน/LU_BKK_2566.shp"
         data = gpd.read_file(filePath)
         self.data = data
-        print(data.shape,'shape of data')
-        print(data.columns)
-        print(data['Area_Sqm'])
-        target_if_more_than_10000 = (self.data['Area_Sqm'] <= 10000)
-        print(target_if_more_than_10000)
+        # print(data.shape,'shape of data')
+        # print(data.columns)
+        # print(data['Area_Sqm'])
+        # target_if_more_than_10000 = (self.data['Area_Sqm'] <= 10000)
+        # print(target_if_more_than_10000)
 
-        iris = datasets.load_iris()
-        X, y = iris.data, iris.target
-
-        clf = svm.SVC(gamma='scale')
-        clf.fit(X, y)
-
-        saved_model = bentoml.sklearn.save_model("iris_clf", clf)
+        # iris = datasets.load_iris()
+        # X, y = iris.data, iris.target
+        #
+        # clf = svm.SVC(gamma='scale')
+        # clf.fit(X, y)
+        #
+        # saved_model = bentoml.sklearn.save_model("iris_clf", clf)
 
     def train(self):
         geometry = self.data['geometry']
@@ -37,6 +37,7 @@ class SVMLandUseClassifier:
         my_df_feature = DataFrame({'area_sqm': area_sqm})
         clf = svm.SVC(gamma='scale',kernel='rbf')
         clf.fit(my_df_feature, target_if_more_than_10000)
+        print('my score ',clf.score(my_df_feature, target_if_more_than_10000))
 
         saved_model = bentoml.sklearn.save_model("area_sqm_svm_clf", clf)
 
