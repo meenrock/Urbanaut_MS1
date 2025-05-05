aed57a4f249a4e729644126b1852f6eb2037f401
sstacker_m <77928053+meenrock@users.noreply.github.com>
2025-05-05 15:54:34 +0700

    asdas

--- service.py
+++ service.py
@@ -1,66 +1,26 @@
-from __future__ import annotations
-
-from typing import Any
-
 import bentoml
+import numpy as np
+from bentoml._internal.io_descriptors import NumpyNdarray
 
-from src.data_processing.models.svm_land_use import SVMLandUseClassifier
+from sklearn import svm
+from sklearn import datasets
 
-with bentoml.importing():
-    from transformers import pipeline
-    import sedona
+# Load training data set
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
 
+# Train the model
+clf = svm.SVC(gamma='scale')
+clf.fit(X, y)
 
-EXAMPLE_INPUT = "Breaking News: In an astonishing turn of events, the small \
-town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, \
-Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' \
-Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped \
-a record-breaking 20 feet into the air to catch a fly. The event, which took \
-place in Thompson's backyard, is now being investigated by scientists for potential \
-breaches in the laws of physics. Local authorities are considering a town festival \
-to celebrate what is being hailed as 'The Leap of the Century."
+# Save model to the BentoML local Model Store
+saved_model = bentoml.sklearn.save_model("iris_clf", clf)
 
+iris_clf_runner = bentoml.sklearn.get("iris_clf:latest").to_runner()
 
-my_image = bentoml.images.PythonImage(python_version="3.11") \
-        .python_packages("torch", "transformers")
+svc = bentoml.Service("iris_classifier", runners=[iris_clf_runner])
 
-svm_clf = SVMLandUseClassifier()
-svm_clf.train()
-
-
-@bentoml.service(
-    image=my_image,
-    resources={"cpu": "2"},
-    traffic={"timeout": 30},
-    http={
-        "cors": {
-            "enabled": True,
-            "access_control_allow_origins": ["http://localhost:4200", "https://myorg.com:8080"],
-            "access_control_allow_methods": ["GET", "OPTIONS", "POST", "HEAD", "PUT"],
-            "access_control_allow_credentials": True,
-            "access_control_allow_headers": ["*"],
-            "access_control_allow_origin_regex": "https://.*\.my_org\.com",
-            "access_control_max_age": 1200,
-            "access_control_expose_headers": ["Content-Length"]
-        }
-    }
-)
-class Summarization:
-    # Define the Hugging Face model as a class variable
-    # test = shp_input.ShapeFileInputSpark()
-    # test.read_shape_file("C:/GIS/input/N_COASTAL_CHANGE_20231203_192816.shp")
-    model_path = bentoml.models.HuggingFaceModel("sshleifer/distilbart-cnn-12-6")
-
-    def __init__(self) -> None:
-        # Load model into pipeline
-        self.pipeline = pipeline('summarization', model=self.model_path)
-
-    @bentoml.api
-    def summarize(self, text: str = EXAMPLE_INPUT) -> dict[Any, str]:
-        result = self.pipeline(text)
-        resultExport = {
-            "result": f"Hello world! Here's your summary: {result[0]['summary_text']}"
-        }
-        return resultExport
-
-    
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def classify(input_series: np.ndarray) -> np.ndarray:
+    result = iris_clf_runner.predict.run(iris.data)
+    return result
--- src/data_processing/models/svm_land_use.py
+++ src/data_processing/models/svm_land_use.py
@@ -4,6 +4,8 @@ from pandas import DataFrame
 
 from sklearn import svm
 
+from sklearn import datasets
+
 class SVMLandUseClassifier:
     def __init__(self):
         filePath = "D:/HighSpeedStorage/LVCHighSpd/MeenMookCoProject/POC/research/land-use-data/Landuse_bkk/กรุงเทพมหานคร2566/การใช้ที่ดิน/LU_BKK_2566.shp"
@@ -14,6 +16,14 @@ class SVMLandUseClassifier:
         print(data['Area_Sqm'])
         target_if_more_than_10000 = (self.data['Area_Sqm'] <= 10000)
         print(target_if_more_than_10000)
+
+        iris = datasets.load_iris()
+        X, y = iris.data, iris.target
+
+        clf = svm.SVC(gamma='scale')
+        clf.fit(X, y)
+
+        saved_model = bentoml.sklearn.save_model("iris_clf", clf)
 
     def train(self):
         geometry = self.data['geometry']
