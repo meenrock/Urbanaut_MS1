67e6c8afaa9bc3646458cbc2f882f46936784e22
sstacker_m <77928053+meenrock@users.noreply.github.com>
2025-05-08 00:06:17 +0700

    dgdghd

---
+++ src/data_processing/models/UNET/Up.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+from src.data_processing.models.UNET.DoubleConv import DoubleConv
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(
+            in_channels, in_channels // 2, kernel_size=2, stride=2
+        )
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(
+            x1,
+            [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2],
+        )
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
